725f36f1630278331da9e29b371f03bc011edda2
faurrahman10 <faturrahmanhasir@gmail.com>
2023-06-04 20:07:49 +0800

    mengganti file register

--- src/pages/auth/Register/index.tsx
+++ src/pages/auth/Register/index.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 
-function Login (): ReactElement {
+function Signup (): ReactElement {
   return (
     <div>
       <div className='d-flex'>
@@ -12,11 +12,35 @@ function Login (): ReactElement {
         <div className='d-flex align-items-center justify-content-center' style={{ width: '50%', height: '100vh' }}>
           <div>
             <form action='' style={{ width: '452px', height: 'fit-content' }}>
-              <h5 style={{ fontWeight: '700', fontSize: '24px', lineHeight: '36px', marginBottom: '24px' }}>Masuk</h5>
+              <h5 style={{ fontWeight: '700', fontSize: '24px', lineHeight: '36px', marginBottom: '24px' }}>Daftar</h5>
+              <div className='name' style={{ marginBottom: '16px' }}>
+                <div className='mb-1'>
+                  <label htmlFor='' style={{ fontWeight: '400', fontSize: '12px', lineHeight: '18px' }}>
+                      Nama
+                  </label>
+                </div>
+                <input
+                  type='text'
+                  placeholder='Nama lengkap'
+                  className='formNama'
+                  style={{
+                    width: '452px',
+                    height: '48px',
+                    fontWeight: '400',
+                    fontSize: '14px',
+                    lineHeight: '20px',
+                    color: '#8A8A8A',
+                    padding: '12px 16px',
+                    outline: 'none',
+                    border: '1px solid #D0D0D0',
+                    borderRadius: '16px'
+                  }}
+                />
+              </div>
               <div className='email' style={{ marginBottom: '16px' }}>
                 <div className='mb-1'>
                   <label htmlFor='' style={{ fontWeight: '400', fontSize: '12px', lineHeight: '18px' }}>
-                      Email/No Telepon
+                      Email
                   </label>
                 </div>
                 <input
@@ -37,18 +61,39 @@ function Login (): ReactElement {
                   }}
                 />
               </div>
+              <div className='noTlp' style={{ marginBottom: '16px' }}>
+                <div className='mb-1'>
+                  <label htmlFor='' style={{ fontWeight: '400', fontSize: '12px', lineHeight: '18px' }}>
+                      Nomor Telepon
+                  </label>
+                </div>
+                <input
+                  type='text'
+                  placeholder='+62'
+                  className='formNoTlp'
+                  style={{
+                    width: '452px',
+                    height: '48px',
+                    fontWeight: '400',
+                    fontSize: '14px',
+                    lineHeight: '20px',
+                    color: '#8A8A8A',
+                    padding: '12px 16px',
+                    outline: 'none',
+                    border: '1px solid #D0D0D0',
+                    borderRadius: '16px'
+                  }}
+                />
+              </div>
               <div className='pass' style={{ marginBottom: '24px' }}>
                 <div className='d-flex mb-1 justify-content-between'>
                   <label htmlFor='' style={{ fontWeight: '400', fontSize: '12px', lineHeight: '18px' }}>
                       Password
                   </label>
-                  <a href='auth/ForgotPassword' style={{ fontWeight: '500', fontSize: '12px', lineHeight: '18px', color: '#7126B5', textDecoration: 'none' }}>
-                      Lupa Kata Sandi
-                  </a>
                 </div>
                 <input
                   type='password'
-                  placeholder='Masukkan password'
+                  placeholder='Buat password'
                   className='formPass'
                   style={{ width: '452px', height: '48px', fontWeight: '400', fontSize: '14px', lineHeight: '20px', padding: '12px 16px', outline: 'none', border: '1px solid #D0D0D0', borderRadius: '16px' }}
                 />
@@ -69,12 +114,12 @@ function Login (): ReactElement {
                   border: 'none'
                 }}
               >
-                  Masuk
+                  Daftar
               </button>
               <p className='text-center' style={{ fontWeight: '400', fontSize: '14px', lineHeight: '20px', color: '#000' }}>
-                  Belum punya akun?{' '}
-                <a href='/auth/Signup' style={{ fontWeight: '700', fontSize: '14px', lineHeight: '20px', color: '#7126B5', textDecoration: 'none' }}>
-                    Daftar di sini
+                  Sudah punya akun?{' '}
+                <a href='/auth/Login' style={{ fontWeight: '700', fontSize: '14px', lineHeight: '20px', color: '#7126B5', textDecoration: 'none' }}>
+                    Masuk di sini
                 </a>
               </p>
             </form>
@@ -85,4 +130,4 @@ function Login (): ReactElement {
   )
 }
 
-export default Login
+export default Signup
