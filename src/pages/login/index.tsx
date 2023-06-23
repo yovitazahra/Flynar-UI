c54323d6de0847ff4da2f3b959fb5b528f10e23b
faurrahman10 <faturrahmanhasir@gmail.com>
2023-06-23 21:16:22 +0800

    integrasi api login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,11 +3,41 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 const Login = (): ReactElement => {
 	const [showPassword, setShowPassword] = useState(false)
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword)
+  }
+
+	const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+	const [errorMessage, setErrorMessage] = useState('')
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/v1/login', { identifier:email, password })
+
+      if (response.status === 200) {
+        const { accessToken } = response.data
+      } else {
+        const { message } = response.data
+        setErrorMessage(message)
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+				if (error.response?.data?.message !== undefined && error.response?.data?.message !== null) {
+					setErrorMessage(error.response.data.message)
+				} else {
+					setErrorMessage('Terjadi kesalahan pada proses registrasi')
+				}
+				console.error(error)
+			} else {
+				console.error(error)
+			}
+    }
   }
 
   return(
@@ -22,7 +52,7 @@ const Login = (): ReactElement => {
 						<div className='flex justify-center'>
 							<Image src='/assets/logoFlynarbaru.png' width={200} height={200} alt='' className='object-contain block lg:hidden' />
 						</div>
-						<form action='' className='w-[110%] h-auto'>
+						<form onSubmit={handleSubmit} action='' className='w-[110%] h-auto'>
 							<h5 className="font-bold text-2xl leading-9 mb-6">Masuk</h5>
 							<div className='email mb-4'>
 								<div className="mb-1">
@@ -30,7 +60,7 @@ const Login = (): ReactElement => {
 										Email/No Telepon
 									</label>
 								</div>
-								<input type="text" placeholder="Contoh: johndoe@gmail.com" className="formEmail w-full h-12 font-normal text-base leading-6 text-gray-600 py-3 px-4 outline-none border border-gray-300 rounded-lg"/>
+								<input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Contoh: johndoe@gmail.com" className="formEmail w-full h-12 font-normal text-base leading-6 text-gray-600 py-3 px-4 outline-none border border-gray-300 rounded-lg"/>
 							</div>
 							<div className="pass mb-6 relative">
 							<div className="flex items-center justify-between mb-1">
@@ -41,7 +71,7 @@ const Login = (): ReactElement => {
 									Lupa Kata Sandi
 								</a>
 							</div>
-							<input type={showPassword ? 'text' : 'password'} placeholder="Masukkan password" id="password" className="formPass relative w-full h-12 font-normal text-base leading-6 py-3 px-4 outline-none border border-gray-300 rounded-lg"/>
+							<input value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? 'text' : 'password'} placeholder="Masukkan password" id="password" className="formPass relative w-full h-12 font-normal text-base leading-6 py-3 px-4 outline-none border border-gray-300 rounded-lg"/>
 							<button
 								type="button" onClick={togglePasswordVisibility} className="bg-white px-2 pr-2 absolute top-1/2 right-2 border-none"
 							>
@@ -51,12 +81,13 @@ const Login = (): ReactElement => {
 						<button type="submit" className="w-full h-12 px-6 py-2 bg-purple-600 rounded-lg text-white font-medium text-base leading-6 mb-10 border-none"> 
 							Masuk
 						</button>
-						<p className="text-center font-normal text-base leading-6 text-black">
+						<p className="text-center font-normal text-base leading-6 text-black mb-3">
 							Belum punya akun?{' '}
 							<Link href="/register" className="font-semibold text-base leading-6 text-purple-700 no-underline">
 								Daftar di sini
 							</Link>
 						</p>
+						{errorMessage && <p className='text-center text-white p-3 bg-red-600 rounded-md w-[100%]'>{errorMessage}</p>}
 						</form>
 					</div>
 				</div>
