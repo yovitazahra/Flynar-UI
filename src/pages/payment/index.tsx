b0228a60ecf73e7a9f2fd8a9018749e8b4c4843a
Hans <classsickpeople@gmail.com>
2023-07-07 20:06:02 +0800

    Create payment page

---
+++ src/pages/payment/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState, type ReactElement } from 'react'
+import DefaultLayout from '@/layouts/default'
+import MenuHeader from '@/components/MenuHeader'
+import { FiEdit3, FiSettings, FiLogOut } from 'react-icons/fi'
+import Head from 'next/head'
+import axios from 'axios'
+import Header from '@/components/Header'
+import { useRouter } from 'next/router'
+import checkLoggedIn from '@/utils/checkLoggedIn'
+
+const Payment = (): ReactElement => {
+  useEffect(() => {
+    const status = checkLoggedIn()
+    setIsLoggedIn(status)
+  }, [])
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <>
+      <Head>
+        <title>Payment</title>
+      </Head>
+      <div id='payment-page' className=''>
+        <Header isLoggedIn={isLoggedIn} login={async () => {}}/>
+        <main className='mx-auto'>
+          <h2>Payment Page</h2>
+        </main>
+      </div>
+    </>
+  )
+}
+
+Payment.getLayout = function getLayout (page: ReactElement) {
+  return (
+    <DefaultLayout>
+      {page}
+    </DefaultLayout>
+  )
+}
+
+export default Payment
