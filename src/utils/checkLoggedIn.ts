e12c5b72ba55f17fac50e5533ef7b07434d7bb47
Hans <classsickpeople@gmail.com>
2023-07-19 23:47:33 +0800

    Delete checkLoggedIn function

--- src/utils/checkLoggedIn.ts
+++
@@ -1,9 +0,0 @@
-const checkLoggedIn = (): boolean => {
-  const accessToken = sessionStorage.getItem('accessToken')
-  if (accessToken !== null) {
-    return true
-  }
-  return false
-}
-
-export default checkLoggedIn
