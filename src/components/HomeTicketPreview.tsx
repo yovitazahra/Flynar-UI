c5ed32082ce1c8b88db333f0a6c13f1b80df9423
Hans <classsickpeople@gmail.com>
2023-08-07 21:52:21 +0800

    Styling HomeTicketPreview component

--- src/components/HomeTicketPreview.tsx
+++ src/components/HomeTicketPreview.tsx
@@ -1,5 +1,8 @@
 import { type ReactElement } from 'react'
 import Image from 'next/image'
+import { FaLongArrowAltRight } from 'react-icons/fa'
+import formatDate from '@/utils/formatDate'
+import formatPrice from '@/utils/formatPrice'
 import type ITicket from '@/types/ticket'
 
 interface IHomeTicketPreview {
@@ -9,18 +12,21 @@ interface IHomeTicketPreview {
 
 const HomeTicketPreview = ({ ticket, createCheckout }: IHomeTicketPreview): ReactElement => {
   return (
-    <div className='cursor-pointer' onClick={() => { createCheckout(`${ticket.id}`) }}>
+    <div className='home-ticket-preview hover-zoom-image cursor-pointer flex flex-col gap-y-3  ' onClick={() => { createCheckout(`${ticket.id}`) }}>
+      <div className='relative h-36 overflow-hidden rounded-xl'>
+        <p className='absolute z-10 top-2 right-2 bg-blue-500 text-sm text-white py-1 pr-2 pl-3 rounded-xl class-seat tracking-wider'>{ticket.classSeat}</p>
+        <Image src={`/images/destination/${ticket.flight.arrivalCity}.jpg`} width={200} height={200} priority={true} alt={ticket.flight.arrivalCity} className='w-full h-full'/>
+      </div>
       <div>
-        <Image src={`/images/destination/${ticket.flight.arrivalCity}.jpg`} width={200} height={200} priority={true} alt={ticket.flight.arrivalCity} className='w-auto h-auto'/>
-        <div className=''>
+        <div className='flex w-full justify-start items-center gap-x-2 font-bold'>
           <p>{ticket.flight.departureCity}</p>
+          <FaLongArrowAltRight />
           <p>{ticket.flight.arrivalCity}</p>
         </div>
-        <p className=''>{ticket.flight.departureDate}</p>
-        <p>{ticket.classSeat}</p>
-        <p className=''>IDR {ticket.price}</p>
+        <p className='font-bold text-cyan-600'>{ticket.flight.airline}</p>
+        <p className='font-semibold'>{formatDate(ticket.flight.departureDate)}</p>
+        <p>Mulai dari IDR <span className='font-bold text-blue-600'>{formatPrice(`${ticket.price}`)}</span></p>
       </div>
-      <div>{ticket.label}</div>
     </div>
   )
 }
